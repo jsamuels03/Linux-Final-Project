5fa5bcf37b236cc73afcb35dd2439ce60232799a
Jaden Samuels <jaden.samuels@bison.howard.edu>
2024-04-28 02:32:47 +0000

    Create Cipher Word variable and loop to go through A-Z

--- mycipher.cpp
+++ mycipher.cpp
@@ -6,22 +6,33 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-int i = 0;
-string message;
-char ch;
-if (argc != 2){
+  int i = 0;
+  string message;
+  char ch;
+  if (argc != 2){
   cout << "Error: Usage cipher <shiftamount>" << endl;
   exit(0);
-}
-getline(cin, message);
-transform(message.begin(), message.end(), message.begin(), ::toupper);
-int key = 2;
-string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string letter = "";
-for(int i=0; i<message.length(); i++){
-  if(isletter(message[i])){
-    letter += message[i];
   }
-}
-
+  getline(cin, message);
+  transform(message.begin(), message.end(), message.begin(), ::toupper);
+  int key = 2;
+  string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  string letter = "";
+  for(int i=0; i<message.length(); i++){
+    if(isletter(message[i])){
+    letter += message[i];
+    }
+  }
+  string cipherWord = "";
+  for(i = 0; letter[i] != '\0'; i++){
+    ch = letter[i];
+    if(ch >= 'A' && ch < 'Z'){
+      ch = ch + key;
+      cipherWord += ch;
+    }
+    if(ch >= 'Z'){
+      ch = 'Z'-24;
+      cipherWord += ch;
+    }
+  }
 }
