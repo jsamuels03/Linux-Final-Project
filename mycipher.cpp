1fef79cc6d0d00c5fe5d8724391b7d9440233e41
Jaden Samuels <jaden.samuels@bison.howard.edu>
2024-04-28 02:09:40 +0000

    Add alphabet and for loop for message

--- mycipher.cpp
+++ mycipher.cpp
@@ -9,5 +9,19 @@ int main(int argc, char* argv[]){
 int i = 0;
 string message;
 char ch;
+if (argc != 2){
+  cout << "Error: Usage cipher <shiftamount>" << endl;
+  exit(0);
+}
+getline(cin, message);
+transform(message.begin(), message.end(), message.begin(), ::toupper);
+int key = 2;
+string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string letter = "";
+for(int i=0; i<message.length(); i++){
+  if(isletter(message[i])){
+    letter += message[i];
+  }
+}
 
 }
